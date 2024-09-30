e528cf9f6c81f6c945c3601c1bc95b1941405f96
Yehyv <yehyaabdu231@gmail.com>
2024-09-30 15:26:32 +0300

    handling CORS errors

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const app = express();
 const morgan = require('morgan'); //middleware for http requests
 const bodyParser = require('body-parser');
 
+//handling CORS errors
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*'); //giving access to any origin you can use it to give access to only your own domain only but it still can be accessed with testing tools like Postman
+    res.header('Acess-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+}); //now you can still send requests from tools like postman and send it from single web page too
+
+
 //Routes for product, order
 const ProductRoutes = require('./api/routes/products');
 const OrderRoutes = require('./api/routes/orders');
